96d87dc8f55c888edc63304ee564c28986f76fe3
Filipp Vasilev <ampa1r.pl@gmail.com>
2019-09-23 21:26:40 +0300

    add create-react-app to project

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+interface IAppState {
+    
+}
+
+class App extends Component<IAppState> {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                <div className="App">
+                        <header className="App-header">
+                            there is an app
+                    </header>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
