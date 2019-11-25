0bbb809e96684ecf3872d0722c4f9be4d3cf7b43
syurov <yourovserg@gmail.com>
2019-11-25 18:36:17 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  
+
+  render() {
+    return (
+      <h1>zdarova</h1>
+    )
+  }
+}
+
+export default App;
